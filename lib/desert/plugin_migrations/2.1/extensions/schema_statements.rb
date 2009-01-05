8411d016e95cbe79264dfadee26768dc7a9d974a
Ayose <ayose@foton.es>
2009-01-05 18:25:16 +0000

    Removed legacy (useless?) code

--- lib/desert/plugin_migrations/2.1/extensions/schema_statements.rb
+++ lib/desert/plugin_migrations/2.1/extensions/schema_statements.rb
@@ -2,27 +2,14 @@ ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
   def initialize_schema_migrations_table_with_plugins
     initialize_schema_migrations_table_without_plugins
 
-    unless ActiveRecord::Base.connection.table_exists?(Desert::PluginMigrations::Migrator.schema_migrations_table_name)
-      execute "CREATE TABLE #{Desert::PluginMigrations::Migrator.schema_migrations_table_name} (plugin_name #{type_to_sql(:string)}, version #{type_to_sql(:string)})"
-      plugins_and_versions = select_all("SELECT plugin_name, version from #{Desert::PluginMigrations::Migrator.schema_info_table_name}")
-      plugins_and_versions.each do |plugin_data|
-        plugin_name, version = plugin_data["plugin_name"], plugin_data["version"]
-        plugin = Desert::Manager.find_plugin(plugin_name)
-        migration_versions = Dir["#{plugin.migration_path}/*.rb"].map do |path|
-          File.basename(path, ".rb")
-        end.select do |migration|
-          Integer(migration.split("_").first) <= Integer(version)
-        end
-        migration_versions.each do |migration_version|
-          insert_sql = ActiveRecord::Base.send(:sanitize_sql, [
-            "INSERT INTO #{Desert::PluginMigrations::Migrator.schema_migrations_table_name}(plugin_name, version) VALUES(?, ?)",
-            plugin_name,
-            Integer(migration_version.split("_").first)
-          ])
-          execute insert_sql
-        end
+    table_name = Desert::PluginMigrations::Migrator.schema_migrations_table_name
+    unless table_exists?(table_name)
+      create_table table_name do |t|
+        t.string :plugin_name
+        t.string :version
       end
     end
   end
+
   alias_method_chain :initialize_schema_migrations_table, :plugins
 end
